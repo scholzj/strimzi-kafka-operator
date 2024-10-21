4a840015bc283124c6d9ff077a8c7de8a4748b11
Jakub Scholz <www@scholzj.com>
2024-10-21 23:52:20 +0200

    Update to RC3
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/model/KafkaBrokerConfigurationBuilder.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/model/KafkaBrokerConfigurationBuilder.java
@@ -355,7 +355,7 @@ public class KafkaBrokerConfigurationBuilder {
         }
 
         // Control plane listener is on all ZooKeeper based brokers, needed during migration as well, when broker still using ZooKeeper but KRaft controllers are ready
-        if (node.broker() && kafkaMetadataConfigState.isZooKeeper()) {
+        if (node.broker() && kafkaMetadataConfigState.isZooKeeperToMigration()) {
             writer.println("control.plane.listener.name=" + CONTROL_PLANE_LISTENER_NAME);
         }
 
